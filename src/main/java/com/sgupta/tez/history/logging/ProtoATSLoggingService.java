7ad0088c7a793351917fee18eef8fae1fc5f9b2a
SHEKHAR GUPTA <sgupta@pepperdata.com>
2020-03-19 17:18:12 -0700

    Adds first set of files

---
+++ src/main/java/com/sgupta/tez/history/logging/ProtoATSLoggingService.java
@@ -0,0 +1,48 @@
+package com.sgupta.tez.history.logging;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.tez.dag.history.DAGHistoryEvent;
+import org.apache.tez.dag.history.logging.ats.ATSHistoryLoggingService;
+import org.apache.tez.dag.history.logging.proto.ProtoHistoryLoggingService;
+
+/**
+ * The logging service for Tez framework to log events to both in ATS and protobuf.
+ * Add the following to tez-site.xml to enable this service.
+ * <property>
+ *   <name>tez.history.logging.service.class</name>
+ *   <value>com.sgupta.tez.history.logging.ProtoATSLoggingService</value>
+ *  </property>
+ */
+public class ProtoATSLoggingService extends ProtoHistoryLoggingService {
+
+  private final ATSHistoryLoggingService atsHistoryLoggingService;
+
+  public ProtoATSLoggingService() {
+    super();
+    atsHistoryLoggingService = new ATSHistoryLoggingService();
+  }
+
+  @Override
+  protected void serviceInit(Configuration conf) throws Exception {
+    super.serviceInit(conf);
+    atsHistoryLoggingService.serviceInit(conf);
+  }
+
+  @Override
+  protected void serviceStart() throws Exception {
+    super.serviceStart();
+    atsHistoryLoggingService.serviceStart();
+  }
+
+  @Override
+  protected void serviceStop() throws Exception {
+    super.serviceStop();
+    atsHistoryLoggingService.serviceStop();
+  }
+
+  @Override
+  public void handle(DAGHistoryEvent event) {
+    super.handle(event);
+    atsHistoryLoggingService.handle(event);
+  }
+}
